606b9129a868c8104390c8622d85ea12a253a877
Zepic <horto@hotmail.no>
2020-09-14 14:48:02 +0200

    Initial commit

---
+++ Piece.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Oblig2
+{
+    abstract class Piece
+    {
+        public string Symbol { get; set; }
+
+        protected Piece(string symbol)
+        {
+            Symbol = symbol;
+        }
+
+        public abstract bool Move(string fromPosition, string toPosition);
+    }
+
+   
+}
---
+++ Rook.cs
@@ -0,0 +1,14 @@
+﻿namespace Oblig2
+{
+    class Rook : Piece
+    {
+        public Rook() : base("Rook")
+        {
+        }
+
+        public override bool Move(string fromPosition, string toPosition)
+        {
+            return fromPosition[0] == toPosition[0] || fromPosition[1] == toPosition[1];
+        }
+    }
+}
